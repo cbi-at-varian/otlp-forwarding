48c06b67ae5f47bce5975c9db06ad5a8d9801ce2
Christian Binder <Christian.Binder@varian.com>
2022-11-23 08:39:07 +0100

    otlp forwarding on fluentbit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # otlp-forwarding
 forwarding opentelemetry with fluentbit
+
+> Disclaimer: Current setup doens't seem to work
+
+# How to reproduce
+
+1. docker-compose up -d
+2. curl  http://localhost:8080/WeatherForecast
+3. find activity id in the logs, e.g. docker-compose logs webapp
+4. open grafana/tempo on http://localhost:3000 and search for the previous found activity id (= trace id)
+
---
+++ otlp-producer/Program.cs
@@ -0,0 +1,37 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// add opentelemetry instrumentation
+builder.Services.AddOpenTelemetryTracing(
+        (builder) => builder
+            .SetResourceBuilder(ResourceBuilder.CreateDefault()
+                .AddService("ServiceA"))
+            .AddAspNetCoreInstrumentation()
+            .AddConsoleExporter()
+            .AddOtlpExporter());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
